092b23a9a6a6b4455800806411cc5febac387276
AbdillahHamzahAli <hamzahjmbg14@gmail.com>
2023-07-07 20:47:08 +0700

    edit card display

--- resources/views/newblog/home.blade.php
+++ resources/views/newblog/home.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-    <section class="pb-10 lg:h-screen">
+    <section class="pb-10 lg:min-h-screen">
         <div class="container">
             <div class="w-full px-4">
                 <div class="mx-auto mb-16 max-w-xl text-center">
@@ -17,7 +17,7 @@
             <div class="flex flex-wrap">
                 @forelse($posts as $post)
                     <div class="w-full px-4 md:w-1/2 xl:w-1/3">
-                        <div class="max-w-md h-[420px] mx-auto bg-white shadow-md overflow-hidden relative">
+                        <div class="max-w-md mb-8 h-[420px] mx-auto bg-white shadow-md overflow-hidden relative">
                             <!-- thumbnail:start -->
                             @if (file_exists(public_path($post->thumbnail)))
                                 <img class="w-full h-48 object-cover object-center" src="{{ asset($post->thumbnail) }}"
@@ -35,10 +35,10 @@
                             </div>
                             <div class="p-4 absolute bottom-0">
                                 <a href="{{ route('blog.post.detail', ['slug' => $post->slug]) }}"
-                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center border-black border-2 hover:bg-black hover:text-white group">
                                     {{ trans('blog.button.read_more.value') }}
-                                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <svg aria-hidden="true" class="group-hover:ml-4 duration-300 w-4 h-4 ml-2 -mr-1"
+                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd"
                                             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                                             clip-rule="evenodd"></path>
--- resources/views/newblog/search_posts.blade.php
+++ resources/views/newblog/search_posts.blade.php
@@ -36,10 +36,10 @@
                             </div>
                             <div class="p-4 absolute bottom-0">
                                 <a href="{{ route('blog.post.detail', ['slug' => $post->slug]) }}"
-                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center border-black border-2 hover:bg-black hover:text-white group">
                                     {{ trans('blog.button.read_more.value') }}
-                                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor"
-                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <svg aria-hidden="true" class="group-hover:ml-4 duration-300 w-4 h-4 ml-2 -mr-1"
+                                        fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd"
                                             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                                             clip-rule="evenodd"></path>
